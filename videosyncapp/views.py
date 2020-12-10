44d630e067f0431eaffc0ff7967b3fba031ec8e7
Atul_Jawa <48586269+Atul-Jawa@users.noreply.github.com>
2020-12-11 00:14:02 +0530

    Update views.py

--- videosyncapp/views.py
+++ videosyncapp/views.py
@@ -1,14 +1,33 @@
 from django.shortcuts import render, redirect
-from .forms import CreateGroup
+from .forms import CreateGroup, CreateGroupgdrive
 from django.contrib import messages
 from django.urls import reverse
-
+from .models import Group
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {'page': 'index', 'include': 'index.html'})
 
 def watchtogether(request, pk):
-    return render(request, 'watchtogether.html', {'page':'watchtogether','grpcode':pk})
+    try:
+        group = Group.objects.get(pk=pk)
+        return render(request, 'watchtogether.html', {'page':'watchtogether', 'grpcode':pk, 'link':group.link})
+    except Group.DoesNotExist:
+        return render(request, 'watchtogether.html', {'page':'watchtogether', 'grpcode':pk})
+
+def creategrpgdrive(request):
+    if request.method == 'POST' :
+        form = CreateGroupgdrive(request.POST, request.FILES)
+        if form.is_valid():
+            user = request.user
+            pk = form.save(user).pk
+            print(pk)
+            messages.success(request, "Group Created Successfully!!")
+            #messages.success(request, "Assignment Uploaded Successfully!!")
+            return redirect(reverse('watchtogether', kwargs={'pk':pk}))
+        else:
+            return render(request, 'creategrp.html', {'form':form})
+    form = CreateGroupgdrive()
+    return render(request, 'creategrp.html', {'page':'creategrp','link':'link', 'form':form})
 
 def creategrp(request):
     if request.method == 'POST' :
